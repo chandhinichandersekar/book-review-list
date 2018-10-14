4747f817e05547cb8be438db724fc3b896bd5d7c
chandhinichandersekar <chandersekar.c@husky.neu.edu>
2018-10-14 12:43:51 -0400

    search type for back end

--- src/runkit/getBooks.test.js
+++ src/runkit/getBooks.test.js
@@ -37,3 +37,21 @@ describe('when the getBooks endpoint is called', () => {
     })
 });
 
+
+describe('with a valid request of search type author', () => {
+    it('should respond back with all the books', async () =>{
+        const mockRequest = {
+            url: "https://find-books-by-title-author-or-isbn-ztnewtvsrs9y.runkit.sh/?text=ch&search=author"
+        };
+        const mockResponse = {
+            writeHead: jest.fn(),
+            end: jest.fn()
+        }; 
+        
+        await endpoint(mockRequest, mockResponse);
+        const books = mockResponse.end.mock.calls[0][0];
+        const jsonBooks = JSON.parse(books);
+        expect(jsonBooks).toMatchSnapshot();
+    })
+});
+
